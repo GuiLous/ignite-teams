a2e3be97ab88f7e475c47b17f0b5575c6cfe29d7
GuilhermeLou22 <guilherme.lourenco@befective.com>
2022-12-14 08:49:19 -0300

    feat: component PlayerCard done
    Componente de listagem de players finalizado

---
+++ src/components/PlayerCard/index.tsx
@@ -0,0 +1,18 @@
+import { ButtonIcon } from '@components/ButtonIcon';
+
+import * as S from './styles';
+
+interface PlayerCardProps {
+  name: string;
+  onRemove: () => void;
+}
+
+export function PlayerCard({ name, onRemove }: PlayerCardProps) {
+  return (
+    <S.Container>
+      <S.Icon name="person" />
+      <S.Name>{name}</S.Name>
+      <ButtonIcon icon="close" type="SECONDARY" onPress={onRemove} />
+    </S.Container>
+  );
+}
